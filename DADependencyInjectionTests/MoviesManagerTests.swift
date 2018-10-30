5dfead86507200411497b77c63664e6c502884d2
VishalVatsal97 <vishalvatsal97@gmail.com>
2018-10-30 11:56:19 +0530

    Update MoviesManagerTests.swift

--- DADependencyInjectionTests/MoviesManagerTests.swift
+++ DADependencyInjectionTests/MoviesManagerTests.swift
@@ -1,11 +1,3 @@
-//
-//  MoviesManagerTests.swift
-//  DADependencyInjection
-//
-//  Created by Dejan on 19/04/2017.
-//  Copyright © 2017 Dejan. All rights reserved.
-//
-
 import XCTest
 @testable import DADependencyInjection
 
@@ -43,4 +35,67 @@ class MoviesManagerTests: XCTestCase {
         }
     }
     
+    func testSearchListItems_NotCached() {
+        let manager = createManager()
+        
+        var result: [ListDisplayable]?
+        
+        let searchItemsExpectation = self.expectation(description: "Search List Items Displayable Expectation")
+        manager.searchListItems(searchTerm: "ghost") { (items) in
+            result = items
+            searchItemsExpectation.fulfill()
+        }
+        self.waitForExpectations(timeout: 0.1) { (error) in
+            guard error == nil else {
+                XCTFail("Expectation error: \(String(describing: error?.localizedDescription))")
+                return
+            }
+            
+            XCTAssertNotNil(result)
+            
+            XCTAssertTrue(result?.count == 5) // Operations demo
+            
+            let item = result?.first
+            XCTAssertEqual(item?.listItemTitle, "Ghost in the Shell")
+        }
+    }
+    
+    func testSearchListItems_Cached() {
+        let manager = createManager()
+        
+        var result: [ListDisplayable]?
+        
+        let getItemsExpectation = self.expectation(description: "Get List Items Displayable Expectation")
+        manager.getListItems { (items) in
+            result = items
+            getItemsExpectation.fulfill()
+        }
+        self.waitForExpectations(timeout: 0.1) { (error) in
+            guard error == nil else {
+                XCTFail("Expectation error: \(String(describing: error?.localizedDescription))")
+                return
+            }
+            
+            XCTAssertNotNil(result)
+        }
+        
+        let searchItemsExpectation = self.expectation(description: "Search List Items Displayable Expectation")
+        manager.searchListItems(searchTerm: "ghost") { (items) in
+            result = items
+            searchItemsExpectation.fulfill()
+        }
+        self.waitForExpectations(timeout: 0.1) { (error) in
+            guard error == nil else {
+                XCTFail("Expectation error: \(String(describing: error?.localizedDescription))")
+                return
+            }
+            
+            XCTAssertNotNil(result)
+            
+            XCTAssertTrue(result?.count == 5) // Operations demo
+            
+            let item = result?.first
+            XCTAssertEqual(item?.listItemTitle, "Ghost in the Shell")
+        }
+    }
 }
